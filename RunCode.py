f6bc2704ff2a4af5152a1a7706fcffefeb81b28f
FloraBiggins <favbiggins1@sheffield.ac.uk>
2019-11-08 14:52:23 +0000

    Computes Real-Time Costs from discrepancy using generated actual load scenarios. Automates identification of variables from results.

--- RunCode.py
+++ RunCode.py
@@ -8,19 +8,25 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 results = pd.read_csv('ef.csv')
+results.columns = ['Stage', 'Node', 'Variable', 'Index', 'Value']
 
-prices = results.iloc[171:1170,4]
+prices = results.loc[results['Variable'] == ' total_prices'].iloc[:,4]
 
-var = results.iloc[119,4]
-cvar = results.iloc[120,4]
-pred_cost = results.iloc[170,4]
 
-no_storage_cost = results.iloc[145,4]
+var = results.loc[results['Variable'] == ' z'].iloc[:,4].reset_index(drop=True)
+cvar = results.loc[results['Variable'] == ' cvar'].iloc[:,4].reset_index(drop=True)
+pred_cost = results.loc[results['Variable'] == ' pred_cost'].iloc[:,4].reset_index(drop=True)
 
-profit = no_storage_cost - prices
-profit_cvar = no_storage_cost - cvar
-profit_var = no_storage_cost - var
-profit_pred_cost = no_storage_cost - pred_cost
+no_storage_cost = results.loc[results['Variable'] == ' no_storage_cost'].iloc[:,4].reset_index(drop=True)
+
+print(no_storage_cost.get_value(0))
+
+profit = no_storage_cost.get_value(0) - prices.array
+profit_cvar = no_storage_cost.get_value(0) - cvar.get_value(0)
+profit_var = no_storage_cost.get_value(0) - var.get_value(0)
+profit_pred_cost = no_storage_cost.get_value(0) - pred_cost.get_value(0)
+
+
 
 """
 num_bins = 40
@@ -36,8 +42,8 @@ plt.title('200 Cost Scenarios and 10 Load Scenarios, Uncertainty = 15%, Alpha=0.
 plt.ylabel('Frequency')
 plt.savefig('Images/Histogram_alpha_95_u_15_b_0p1.pdf')
 plt.show()
-"""
 
+"""
 
 num_bins = 40
 n, bins, patches = plt.hist(profit, num_bins, facecolor='blue', alpha=0.4)
@@ -45,8 +51,8 @@ plt.axvline(profit_cvar, color='k', linestyle='dashed', linewidth=1)
 plt.text(profit_cvar, 30, 'CVaR', fontsize=10, bbox = {'facecolor' : 'white'}) #: {:.2f}.format(cvar)
 plt.axvline(profit_var, color='k', linestyle='dashed', linewidth=1)
 plt.text(profit_var, 40, 'VaR', fontsize=10, bbox = {'facecolor' : 'white'})
-plt.axvline(profit_pred_cost, color='r', linestyle='dashed', linewidth=1)
-plt.text(profit_pred_cost, 5, 'Predicted \n Profit', fontsize=10, color='red')
+#plt.axvline(profit_pred_cost, color='r', linestyle='dashed', linewidth=1)
+#plt.text(profit_pred_cost, 5, 'Predicted \n Profit', fontsize=10, color='red')
 plt.xlabel('Day-Ahead Market Generated Scenarios (£)')
 #plt.title('1000 Cost Scenarios, Uncertainty = 5%, Alpha=0.95, Beta=0.1', fontsize=10)
 plt.ylabel('Frequency')
@@ -55,6 +61,3 @@ plt.show()
 
 print(profit_cvar)
 
-load = results.iloc[121:145,4:5].reset_index(drop=True)
-
-print(load)
--- models/ReferenceModel.py
+++ models/ReferenceModel.py
@@ -2,6 +2,7 @@
 from pyomo.environ import *
 import random
 import numpy as np
+import matplotlib.pyplot as plt
 
 #
 # Model
@@ -31,7 +32,7 @@ model.efficiency_d = Param(model.i, within=PositiveReals)
 
 model.l_pred = Param(model.T)
 
-model.l_act = Param(model.T)
+#model.l_act = Param(model.T)
 
 model.p_da_pred = Param(model.T, within=PositiveReals)
 
@@ -43,9 +44,9 @@ model.beta = Param(within=PositiveReals)
 
 model.uncertainty = Param(within=PositiveReals)
 
-price_scenarios = 1000
+price_scenarios = 50
 
-load_scenarios = 1
+load_scenarios = 20
 
 #
 # Variables
@@ -67,6 +68,7 @@ model.d = Var(model.T, model.i, bounds=power_bounds_rule)
 
 model.u_sch = Var(model.T)
 
+model.l_act = Var(model.T)
 
 # Price Variables
 
@@ -78,20 +80,21 @@ model.prices = Var(range(price_scenarios), range(load_scenarios))
 
 model.prices_rt = Var(range(price_scenarios), range(load_scenarios))
 
+model.total_prices = Var(range(price_scenarios), range(load_scenarios))
+
 model.pred_cost = Var()
 
 model.prices_no_storage = Var(range(price_scenarios), range(load_scenarios))
 
 model.no_storage_cost = Var()
 
+model.da_pred_prices = Var(range(price_scenarios), model.T)
 
 # Load Variables
 
 model.load_scenarios = Var(range(load_scenarios), model.T)
 
 model.load_scenarios_no_storage = Var(range(load_scenarios), model.T)
-
-model.l_act = Var(model.T)
 
 model.load_diff_rt = Var(range(load_scenarios), model.T)
 
@@ -146,15 +149,20 @@ def random_price_matrix_rule(model, s, t):
 
 model.price_scenarios = Param(range(price_scenarios), model.T, initialize=random_price_matrix_rule)
 
+def get_da_pred_prices_rule(model, s, t):
+    return model.da_pred_prices[s,t] == model.price_scenarios[s,t]
+
+model.get_da_pred_prices = Constraint(range(price_scenarios), model.T, rule=get_da_pred_prices_rule)
+
 def real_time_price_rule(model, t):
     return model.p_da_pred[t] * random.uniform(0.5, 1.5) + \
-           random.uniform( - 0.1 * model.p_da_pred[t], 0.1 * model.p_da_pred[t])
+           random.uniform( - 0.1, 0.1 )  * model.p_da_pred[t]
 
 model.p_rt = Param(model.T, initialize=real_time_price_rule)
 
 def real_time_price_matrix_rule(model, s, t):
     return model.p_da_pred[t] * random.uniform(0.5, 1.5) + \
-           random.uniform( - 0.1 * model.p_da_pred[t], 0.1 * model.p_da_pred[t])
+           random.uniform( - 0.1 , 0.1 ) * model.p_da_pred[t]
 
 model.p_rt_scenarios = Param(range(price_scenarios), model.T, initialize=real_time_price_matrix_rule)
 
@@ -169,8 +177,7 @@ model.p_da_act_constraint = Constraint(model.T, rule=random_price_rule)
 
 def random_load_matrix_rule(model, s, t):
     return model.load_scenarios[s, t] == (model.l_pred[t] * random.uniform(1 - model.uncertainty, 1 + model.uncertainty)
-                                          + random.uniform(- model.uncertainty, model.uncertainty))/1000 + \
-           sum(model.c[t, i] - model.efficiency_d[i] * model.d[t, i] for i in model.i)
+                                          + random.uniform(- model.uncertainty, model.uncertainty))/1000
 
 model.load_scenarios_constraint = Constraint(range(load_scenarios), model.T, rule=random_load_matrix_rule)
 
@@ -185,13 +192,18 @@ model.l_act_constraint = Constraint(model.T, rule=random_load_rule)
 # Total Cost Scenario Matrix for All Possible Loads and Prices
 
 def prices_calculation_rule(model, s1, s2):
-    return model.prices[s1,s2] == sum(model.price_scenarios[s1,t] * model.load_scenarios[s2,t] for t in model.T)
+    return model.prices[s1,s2] == sum(model.price_scenarios[s1,t] * (model.load_scenarios[s2,t] +
+                                                                     sum(model.c[t,i] - model.efficiency_d[i] *
+                                                          model.d[t,i] for i in model.i))
+                                      for t in model.T)
 
 model.price_calculations = Constraint(range(price_scenarios), range(load_scenarios), rule=prices_calculation_rule)
 
 def cost_expectation_rule(model):
     #return model.pred_cost == sum(model.u_sch[t] * model.p_da_pred[t] for t in model.T)
     return model.pred_cost == sum(model.prices[s1,s2] for s1 in range(price_scenarios)
+                                  for s2 in range(load_scenarios))/(price_scenarios*load_scenarios) + \
+           sum(model.prices_rt[s1,s2] for s1 in range(price_scenarios)
                                   for s2 in range(load_scenarios))/(price_scenarios*load_scenarios)
 
 model.cost_expectation = Constraint(rule=cost_expectation_rule)
@@ -199,27 +211,34 @@ model.cost_expectation = Constraint(rule=cost_expectation_rule)
 
 def cost_expectation_no_storage_rule(model):
     return model.no_storage_cost == sum(model.price_scenarios[s1,t] * model.l_pred[t]/1000 for t in model.T
+                                        for s1 in range(price_scenarios))/price_scenarios + \
+           sum(model.p_rt_scenarios[s1,t] * (model.l_act[t] - model.l_pred[t])/1000 for t in model.T
                                         for s1 in range(price_scenarios))/price_scenarios
+
 
 model.cost_expectation_no_storage = Constraint(rule=cost_expectation_no_storage_rule)
 
 # Real-Time Cost Scenarios
 
 def real_time_matrix_rule(model, s, t):
-    return model.load_diff_rt[s,t] == model.load_scenarios[s,t] - model.l_act[t]
+    return model.load_diff_rt[s,t] == model.l_pred[t]/1000 - model.load_scenarios[s,t]
 
-model.real_time_matrix_constraint = Constraint(range(load_scenarios), model.T)
+model.real_time_matrix_constraint = Constraint(range(load_scenarios), model.T, rule=real_time_matrix_rule)
 
 def prices_rt_calculation_rule(model, s1, s2):
     return model.prices_rt[s1,s2] == sum(model.p_rt_scenarios[s1,t] * model.load_diff_rt[s2,t] for t in model.T)
 
-model.price_rt_calculations = Constraint(range(price_scenarios), range(load_scenarios), rule=prices_calculation_rule)
+model.price_rt_calculations = Constraint(range(price_scenarios), range(load_scenarios), rule=prices_rt_calculation_rule)
 
+def total_price_calculation(model, i, j):
+    return model.total_prices[i,j] == model.prices[i,j] + model.prices_rt[i,j]
+
+model.total_prices_constraint = Constraint(range(price_scenarios), range(load_scenarios), rule=total_price_calculation)
 
 # CVaR Constraints and Calcuation
 
 def cvar_first_rule(model, i, j):
-    return model.prices[i,j] - model.z - model.y[i,j] <= 0
+    return model.total_prices[i,j] - model.z - model.y[i,j] <= 0
 
 model.cvar_first_constraint = Constraint(range(price_scenarios), range(load_scenarios), rule=cvar_first_rule)
 
@@ -246,10 +265,14 @@ def ComputeFirstStageCost_rule(model):
 model.FirstStageCost = Expression(rule=ComputeFirstStageCost_rule)
 
 def ComputeSecondStageCost_rule(model):
-    return sum(model.u_sch[t] * model.p_da_act[t] for t in model.T) + \
-           sum((model.l_act[t]/1000 - model.u_sch[t]) * model.p_rt[t] for t in model.T)
+    return sum(model.u_sch[t] * model.p_da_act[t] for t in model.T)
 
 model.SecondStageCost = Expression(rule=ComputeSecondStageCost_rule)
+
+def ComputeThirdStageCost_rule(model):
+    return sum((model.l_act[t]/1000 - model.u_sch[t]) * model.p_rt[t] for t in model.T)
+
+model.ThirdStageCost = Expression(rule=ComputeThirdStageCost_rule)
 
 
 #
@@ -264,225 +287,3 @@ def day_ahead_obj_rule(model):
 
 model.day_ahead_rule = Objective(rule=day_ahead_obj_rule, sense=minimize)
 
-
-"""
-no_storage = 0
-
-p_rt_pred = [55.138609,
-60.941038,
-58.140177,
-43.713802,
-58.184362,
-71.822004,
-63.774196,
-65.221001,
-66.161911,
-86.264506,
-69.463536,
-63.759761,
-63.517166,
-58.383306,
-32.472592,
-56.892780,
-69.687804,
-73.508857,
-68.715289,
-69.340769,
-54.279267,
-32.369939,
-72.153706,
-38.294100]
-
-l_act = [41110.57068089, 37262.07534591, 35530.42694912, 33613.90907861,
- 33614.9218241,  35880.0624191,  46758.42342253, 57339.9140661,
- 58839.32771865, 57283.8712273,  54769.0068911,  54034.56813284,
- 55757.6658119,  53919.67918116, 54983.20504804, 61039.55217808,
- 76539.19466524, 90168.43503743, 90658.54724467, 83425.9052774,
- 76780.84903932, 69186.06323457, 57632.92706161, 42895.4987654 ]
-
-u_act3 = p_rt_pt_pred = np.array([])
-
-socj = np.array([])
-for i in range(no_storage + 1):
-    socj = np.append(socj, 0.4 * 1)
-
-charge_rt = [[] for i in range(no_storage + 1)]
-discharge_rt = [[] for i in range(no_storage + 1)]
-soc_rt = [[] for i in range(no_storage + 1)]
-
-u_act3_pm = p_rt_pm_pred = p_rt_pm_act = np.array([])
-
-socj_pm = np.array([])
-for i in range(no_storage + 1):
-    socj_pm = np.append(socj_pm, 0.4 * 1)
-
-charge_rt_pm = [[] for i in range(no_storage + 1)]
-discharge_rt_pm = [[] for i in range(no_storage + 1)]
-soc_rt_pm = [[] for i in range(no_storage + 1)]
-
-for i in range(no_storage + 1):
-    soc_rt[i] = np.append(soc_rt[i], [socj[i]])
-    soc_rt_pm[i] = np.append(soc_rt_pm[i], [socj_pm[i]])
-
-l = 0
-
-while l <= 23:
-
-    
-    Strategy 3 (MPC Optimisation)
-    
-
-    if l == 24:
-        j = 0
-    else:
-        j = l
-
-    model = ConcreteModel(name="RealTime")
-    model.T = RangeSet(0, 23)
-    model.i = RangeSet(0, no_storage)
-
-    # INDICES
-
-    k = np.array([])  # future time periods
-    for t in model.T:
-        m = j + 1 + t
-        if m > 23:
-            m = m - 24
-        k = np.append(k, m)
-    k = k.astype(int)
-    model.n = Var(model.T)
-    model.m = Var(model.T)
-
-
-    # VARIABLES
-
-    def power_bounds_rule(model, t, i):
-        return (0, 2 * 1)
-
-
-    def capacity_bounds_rule(model, t, i):
-        return (0.4 * 1, 1 * 1)
-
-
-    model.c = Var(model.T, model.i, bounds=power_bounds_rule)
-    model.d = Var(model.T, model.i, bounds=power_bounds_rule)
-    model.x = Var(model.T, model.i, bounds=capacity_bounds_rule)
-    model.u_act = Var(model.T)
-    model.u_pred = Var(model.T)
-    model.p_pm_pred = Var(model.T)
-
-
-    # FUNCTIONS
-
-    def soc_rule_current_period(model, i):
-        return model.x[j, i] == socj[i] + 0.95 * model.c[j, i] - model.d[j, i]
-
-
-    def soc_rule_current_period_pm(model, i):
-        return model.x[j, i] == socj_pm[i] + 0.95 * model.c[j, i] - model.d[j, i]
-
-
-    def soc_rule(model, t, i):
-        if t == 0:
-            s = 23
-        else:
-            s = t - 1
-        return model.x[k[t], i] == model.x[k[s], i] + 0.95 * model.c[k[t], i] - model.d[k[t], i]
-
-
-    def power_import_current_period(model):
-        return model.u_act[j] == (l_act[j] / 1000) + sum(model.c[j, i] - 0.85 * model.d[j, i] for i in model.i)
-
-
-    def power_import_predicted_periods(model, t):
-        return model.u_pred[k[t]] == (model.l_pred[k[t]] / 1000) + sum(
-            model.c[k[t], i] - 0.85 * model.d[k[t], i] for i in model.i)
-
-
-    def absolute_val_max(model, t):
-        return model.u_act[t] - model.u_sch[t] <= model.n[t]
-
-
-    def pred_absolute_val_max(model, t):
-        return model.u_pred[t] - model.u_sch[t] <= model.m[t]
-
-
-    def strat3_obj_rule(model):
-        return ((model.n[j] * p_rt_pred[j]) + sum(model.m[k[t]] * p_rt_pred[k[t]] for t in range(23)))
-
-
-    def p_pm_current_rule(model):
-        return model.p_pm_pred[j] == p_rt_pred[j] + model.gradient * (model.n[j])
-
-
-    def p_pm_rule(model, t):
-        return model.p_pm_pred[k[t]] == p_rt_pred[k[t]] + model.gradient * (model.m[k[t]])
-
-
-    def real_time_obj_rule_pm(model):
-        return ((model.n[j] * model.p_pm_pred[j]) + sum(model.m[k[t]] * model.p_pm_pred[k[t]] for t in range(23)))
-
-
-    # CONSTRAINTS
-
-    model.soc_current_period_constraint = Constraint(model.i, rule=soc_rule_current_period)
-    model.soc_current_period_pm_constraint = Constraint(model.i, rule=soc_rule_current_period_pm)
-    model.soc_constraint = Constraint(range(23), model.i, rule=soc_rule)
-    model.absolute_val_max_constraint = Constraint(model.T, rule=absolute_val_max)
-    model.pred_absolute_val_max_constraint = Constraint(model.T, rule=pred_absolute_val_max)
-    model.power_predicted_constraint = Constraint(range(23), rule=power_import_predicted_periods)
-    model.power_current_constraint = Constraint(rule=power_import_current_period)
-    model.p_pm_constraint = Constraint(range(23), rule=p_pm_rule)
-    model.p_pm_current_constraint = Constraint(rule=p_pm_current_rule)
-
-    # OBJECTIVE
-
-    model.real_time_objective_pm = Objective(rule=real_time_obj_rule_pm)
-    model.real_time_strat_3_objective = Objective(rule=strat3_obj_rule)
-
-    # STRATEGY 3 SOLVE
-
-    model.real_time_objective_pm.deactivate()
-    model.soc_current_period_pm_constraint.deactivate()
-
-    solver_result = solver.solve(model)
-    for i in model.i:
-        charge_rt[i] = np.append(charge_rt[i], [model.c[j, i].value])
-        discharge_rt[i] = np.append(discharge_rt[i], [model.d[j, i].value])
-        soc_rt[i] = np.append(soc_rt[i], [model.x[j, i].value])
-        socj[i] = model.x[j, i].value
-
-    u_act3 = np.append(u_act3, model.u_act[j].value)
-    p_rt_pt_pred = np.append(p_rt_pt_pred, model.p_pm_pred[j].value)
-
-    # PRICE MAKER SOLVE
-
-    model.real_time_strat_3_objective.deactivate()
-    model.real_time_objective_pm.activate()
-    model.soc_current_period_constraint.deactivate()
-    model.soc_current_period_pm_constraint.activate()
-
-    solver_result = solver.solve(model)
-    for i in model.i:
-        charge_rt_pm[i] = np.append(charge_rt_pm[i], [model.c[j, i].value])
-        discharge_rt_pm[i] = np.append(discharge_rt_pm[i], [model.d[j, i].value])
-        soc_rt_pm[i] = np.append(soc_rt_pm[i], [model.x[j, i].value])
-        socj_pm[i] = model.x[j, i].value
-
-    p_rt_pm_pred = np.append(p_rt_pm_pred, model.p_pm_pred[j].value)
-    p_rt_pm_act = np.append(p_rt_pm_act, p_rt_act[j] + gradient_rt *
-                            (l_act[j] / 1000 + sum(model.c[j, i].value - 0.85 * model.d[j, i].value for i in model.i)))
-
-    u_act3_pm = np.append(u_act3_pm, model.u_act[j].value)
-
-    l = l + 1
-
-for i in range(no_storage + 1):
-    soc_rt[i] = np.delete(soc_rt[i], -1)
-    soc_rt_pm[i] = np.delete(soc_rt_pm[i], -1)
-
-charge_total_rt = sum(charge_rt[i] for i in range(no_storage + 1))
-discharge_total_rt = sum(discharge_rt[i] for i in range(no_storage + 1))
-soc_total_rt = sum(soc_rt[i] for i in range(no_storage + 1))
-
-"""
